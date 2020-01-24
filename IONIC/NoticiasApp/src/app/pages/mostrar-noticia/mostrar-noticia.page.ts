604bafd71c64ddba7b4f68c5c9bddb5bf45bdfc1
guillem.burnlees.viada <guillem.burnlees.viada@outlook.es>
2020-01-24 12:13:16 +0100

    ionic progress

--- IONIC/NoticiasApp/src/app/pages/mostrar-noticia/mostrar-noticia.page.ts
+++ IONIC/NoticiasApp/src/app/pages/mostrar-noticia/mostrar-noticia.page.ts
@@ -10,6 +10,17 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class MostrarNoticiaPage implements OnInit {
 
+    temp: Noticia = {
+        id: 0,
+        imagen: "http://paddlevan.es/wp-content/uploads/2017/08/sinimagen.png",
+        titulo: "",
+        subtitulo: "",
+        seccion: "",
+        autor: "",
+        fecha: new Date(),
+        contenido: ""
+    };
+
     noticia: Noticia = {
         id: 0,
         imagen: "http://paddlevan.es/wp-content/uploads/2017/08/sinimagen.png",
@@ -24,21 +35,40 @@ export class MostrarNoticiaPage implements OnInit {
   constructor(private noticiasService: NoticiasService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    const params = this.activatedRoute.snapshot.params;
-    if (params.id) {
-      this.noticiasService.getNoticia(params.id)
-          .subscribe(
-              res => {
-                console.log(res);
-                this.noticia = res;
-                  console.log(this.noticia);
-                  if (this.noticia.imagen == ""){
-                      this.noticia.imagen = "http://paddlevan.es/wp-content/uploads/2017/08/sinimagen.png";
-                  }
-              },err => console.error(err)
-          ) }
+
+    this.getNoticia();
 
   }
+
+  getNoticia(){
+      const params = this.activatedRoute.snapshot.params;
+      if (params.id) {
+          this.noticiasService.getNoticia(params.id)
+              .subscribe(
+                  res => {
+                      console.log(res);
+                      this.noticia = res;
+                      console.log(this.noticia);
+                      if (this.noticia.imagen == ""){
+                          this.noticia.imagen = "http://paddlevan.es/wp-content/uploads/2017/08/sinimagen.png";
+                      }
+                  },err => console.error(err)
+              ) }
+  }
+
+    doRefresh(event) {
+        console.log('Begin async operation');
+
+        setTimeout(() => {
+            console.log('Async operation has ended');
+
+            this.noticia = this.temp;
+
+            this.getNoticia();
+
+            event.target.complete();
+        }, 2000);
+    }
 
 
 
