8fd360e989800b23a0bf25ed3abab3a66a17e759
Noruka <guillem.burnlees.viada@cieep.com>
2020-01-25 12:46:06 +0100

    project coments

--- IONIC/NoticiasApp/src/app/pages/inicio/inicio.page.ts
+++ IONIC/NoticiasApp/src/app/pages/inicio/inicio.page.ts
@@ -13,13 +13,15 @@ import {IonInfiniteScroll, MenuController} from "@ionic/angular";
 })
 export class InicioPage implements OnInit {
 
-    pipo = 0;
-    contador = 0;
-    noticias: Noticia[];
-    componentes: Observable<Componente[]>;
+
+    mostrar = 0; /*variable para el infinite scroll para saber cuantos elementos mostrar*/
+    contador = 0; /*contador para saber que noticia hay que leer siguiente de la lista*/
+    noticias: Noticia[]; /*array de todas las noticias*/
+    componentes: Observable<Componente[]>; /*array de componentes del menu*/
+    noticiasAMostrar: 5; /*Aqui se especifican las noticias que se mostraran en cada refresh del infinite scroll*/
 
     @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll: IonInfiniteScroll;
-    data: Noticia[] = [];
+    data: Noticia[] = []; /*array de noticias que se estan mostrando en el infinite scroll*/
 
     constructor(private noticiasService: NoticiasService, private menuCtrl: MenuController, @Inject(DataService) private dataService: DataService) {
 
@@ -27,55 +29,65 @@ export class InicioPage implements OnInit {
 
     ngOnInit() {
 
-        this.componentes = this.dataService.getMenuOpts();
+        this.componentes = this.dataService.getMenuOpts(); /* carga las opciones del menu */
 
-        this.getNoticias();
+        this.getNoticias(); /*carga la lista de noticias de la api*/
 
-        this.loadData(null);
+        this.loadData(null); /*carga 5 elementos al infinite scroll*/
     }
 
+    /*funcion para refrescar la pagina completamente*/
     doRefresh(event) {
         console.log('Begin async operation');
 
         setTimeout(() => {
             console.log('Async operation has ended');
-
+            /*Borra las listas y resetea la informacion para cargar la pagina como si la iniciaras de 0*/
             this.noticias = [];
             this.data = [];
             this.contador = 0;
-            this.pipo = 0;
+            this.mostrar = 0;
 
 
 
-            this.getNoticias();
-            this.loadData(null);
+            this.getNoticias(); /*vuelve a cargar la lista de noticias de la api. Esta funcion realmente la uso por si he eliminado o modificado una noticia*/
+            this.loadData(null); /*carga elementos iniciales*/
 
 
-            event.target.complete();
+            event.target.complete(); /*deja de ejecutar la animacion del infinite scroll*/
         }, 2000);
     }
 
+    /*funcion para cargar la informacion al infinite scroll*/
     loadData(event) {
         console.log('Cargando siguientes ...');
         setTimeout(() => {
 
-        this.getNoticias();
+        this.getNoticias(); /*por si acaso refresca la lista local de noticias*/
 
-        if ( this.data.length > this.noticias.length ) {
+
+            /*Aqui comparo las noticias que estoy mostrando (data) y las que me faltan por mostrar(noticias).
+             Si las noticias que estoy mostrando(data) es mayor o igual a la lista de todas las noticias(noticias)
+             entonces desactivara el infinite scroll. En caso de que quieras cargar noticias que añadas apartir de este momento
+             puedes refrescar la pagina arriba del todo y pues se recargara la lista local*/
+        if ( this.data.length >= this.noticias.length ) {
             event.target.complete();
             this.infiniteScroll.disabled = true;
             return;
         }
 
-        if (this.noticias.length - this.data.length >= 5) {
-            this.pipo = 5;
+        /*si la diferencia de la lista total, y la lista que estas mostrando es mayor o igual a 5, entonces se mostraran 5
+        * si es menos entonces se mostrara la diferencia. Asi evito las cards vacias*/
+        if (this.noticias.length - this.data.length >= this.noticiasAMostrar) {
+            this.mostrar = this.noticiasAMostrar;
         } else {
-            this.pipo = this.noticias.length - this.data.length;
+            this.mostrar = this.noticias.length - this.data.length;
         }
 
-        const nuevoArr = Array(this.pipo);
+        /*Aqui creo el nuevo array con la longitud de noticias que voy a mostrar. Si son 5 o si son menos se especifica aqui*/
+        const nuevoArr = Array(this.mostrar);
         for (let i = 0; i < nuevoArr.length; i++) {
-            if (this.contador < this.noticias.length) {
+            if (this.contador < this.noticias.length) { /*comprueba que aun faltan noticias por mostrar*/
             nuevoArr[i] = this.noticias[this.contador];
             this.contador++;
             }
